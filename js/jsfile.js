3db9f790211de415ce6fe05725cedf95c5faa13d
W0rpy <114204815+W0rpy@users.noreply.github.com>
2022-09-23 18:35:11 +0500

    Add files via upload

---
+++ js/jsfile.js
@@ -0,0 +1,8 @@
+
+document.querySelector('input');
+let button = document.querySelector('input');
+button.onsubmit = Function()
+{
+	button.classList.remove('.button');
+	button.classList.add('input');
+};
